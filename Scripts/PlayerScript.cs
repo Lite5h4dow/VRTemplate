c7d5a9faa0b6d16fb89d500c0364968671bbcf00
Lamar Daughma <tony@shadowlabs.studio>
2022-08-19 23:53:53 +0100

    adding project files

---
+++ Scripts/PlayerScript.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public class PlayerScript : RigidBody {
+  private ARVRCamera camera;
+  private ARVROrigin origin;
+  private CollisionShape collider;
+
+  // Called when the node enters the scene tree for the first time.
+  public override void _Ready() {
+    camera = GetNode<ARVRCamera>("FPController/ARVRCamera");
+    origin = GetNode<ARVROrigin>("FPController");
+    collider = GetNode<CollisionShape>("Collider");
+  }
+
+  // Called every frame. 'delta' is the elapsed time since the previous frame.
+  public override void _Process(float delta) {
+    AlignCamera(camera, origin, this);
+  }
+
+  private static void AlignCamera(ARVRCamera camera, ARVROrigin origin, RigidBody player) {
+    Vector3 offset = camera.GlobalTranslation - player.GlobalTranslation;
+    origin.GlobalTranslation += offset.Inverse();
+    camera.GlobalTranslation += offset;
+
+  }
+}
